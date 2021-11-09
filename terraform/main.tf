6c36976f842fa57bb92c69d235220d3fd26f30a2
heorhiisatler <sgv1984@gmail.com>
2021-11-09 10:21:08 +0200

    09.11.2021 10:21

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,13 +10,21 @@ terraform {
 provider "proxmox" {
     pm_api_url = "https://192.168.5.251:8006/api2/json"
     pm_tls_insecure = true
-    pm_api_token_id = "tf_user@pve!terraform"
-    pm_api_token_secret  = "585dfaad-f6f3-4c75-acbc-5820a7fa4ea8"
+    #pm_api_token_id = "tf_user@pve!terraform"
+    #pm_api_token_secret  = "585dfaad-f6f3-4c75-acbc-5820a7fa4ea8"
+    pm_api_token_id = var.pm_api_token_id
+    pm_api_token_secret = var.pm_api_token_secret
+
+
 }
 
 variable "ssh_key" {
   default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDNsGvHB22PNhkpEEbgmpZBCBIq8KoGAMM+B1W2oaNhzK7nglte8wdttRthSUlaWB5aIfOCmspLRIaLPZ2Rg8/VkJA2xXPE5v9TIIQvkYOCKQ3xr7i4Uj0UFmGQX7YnFfs3dDBhLMObPU1bKz34NgwKW2xosidWFS8+xt0BmxBNqmTvwmr9YV0aqH0LJHyY2l7VYL6IVuGAViqrUvhPUXSeYRqylr2PVmrkOJMFOUYb76q9I4GF5wswJo75tFjSubqSuZ/IhMlhZvb+ZESE3jS4+ApMch3kD5hg719dG0RiwXaMHl4CzP+FfGy5JsAYi0mleuuh/d2F7H3MOS9ECsLAEAIhkRxWafA1j1n+OMuqHIeT8tJbFVQa/QQ3u1f9c/6Vy9SgC4V/nFCYcPuz8GevzgSG7Pd0C9ZNLPxb23gajTdTEvLEzk56/QeAIvoifLM+fNrZIaATutoC5ACzY/AJ5KyHl+Z/7dlmX1zwCnsRXYwMto5lrYcmVTsylg7xYb0= decepticon@control-srv.lan"
 }
+
+variable "pm_api_token_id" {}
+variable "pm_api_token_secret" {}
+
 
 resource "proxmox_vm_qemu" "proxmox_vm" {
   count             = 1
