4ab098965022fad774a9876511163d2e145a9b5a
heorhiisatler <sgv1984@gmail.com>
2021-11-30 17:42:23 +0200

    fix

---
+++ terraform/aws/main.tf
@@ -0,0 +1,64 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-3"
+}
+
+resource "aws_key_pair" "alice" {
+  key_name   = "alice"
+  public_key = file("./keys/is_rsa.pub")
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+resource "aws_instance" "alice" {
+  key_name      = aws_key_pair.alice.key_name
+  
+  ami           = "ami-0df7d9cc2767d16cd"
+  instance_type = "t2.micro"
+
+  vpc_security_group_ids = [ aws_security_group.allow_ingress.id, aws_security_group.allow_egress.id ]
+}
+
+resource "aws_security_group" "allow_ingress" {
+  vpc_id      = data.aws_vpc.default.id
+  
+  name        = "allow_ingress"
+  description = "Allow ingress"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "allow_egress" {
+  vpc_id      = data.aws_vpc.default.id
+
+  name        = "allow_egress"
+  description = "Allow egress"
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_eip" "ip" {
+  vpc      = true
+  instance = aws_instance.alice.id
+}
